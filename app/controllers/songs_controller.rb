28135e757bc63620fa9cd01fc174ac9614813f31
Doug <djmacdonald@uchicago.edu>
2015-01-30 15:55:47 -0500

    Added Song resource

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,8 @@
+class SongsController < ApplicationController
+
+  def index
+    @album = Album.find(params[:album_id])
+    @songs = @album.songs
+  end
+
+end
--- app/models/album.rb
+++ app/models/album.rb
@@ -1,5 +1,5 @@
 class Album < ActiveRecord::Base
-
+  has_many :songs
   #  defining a class constant named GENRES
   # Album::GENRES to access outside of the class
   GENRES = %w{rock rap country jazz ska dance}

cf3193fa619b51b9489a3bdb10a30449ec2821bb
Doug <djmacdonald@uchicago.edu>
2015-01-30 14:06:52 -0500

    Added Album

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Album.delete_all
+
+nevermind = Album.create!(title: "Nevermind", genre: 'rock')
+sea_change = Album.create!(title: "Sea Change", genre: 'jazz')
